cae227047a761607679f639bff61575260b13414
Cameron Moreau <cameron.cm6@gmail.com>
2016-09-16 17:27:09 -0500

    Initial Commit

---
+++ MavSwipes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MavSwipes
+//
+//  Created by Cameron Moreau on 9/16/16.
+//  Copyright © 2016 Mobi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
